b295d0078b0cb0b0fc07bef14e7bf6a67dfdde7e
Thymios <douvisthym@gmail.com>
2018-11-05 16:09:32 +0200

    Set up fetch expenses

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
-} else if (process.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
 }
 
